7986408ade3adbc21dd24d74b42c73afceff04fb
dev-am4 <am4.production29@gmail.com>
2026-09-10 12:53:28 +0700

    Speed up touchscreen quiz rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './fast-runtime'
 import App from './App'
 import './styles.css'
 import './mobile.css'
 import './review-enhancer.css'
 import './review-enhancer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
