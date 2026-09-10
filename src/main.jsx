76791f27ba0b4ec225dfb0e0c1a70ea2737bd5e6
dev-am4 <am4.production29@gmail.com>
2026-09-10 12:36:48 +0700

    Enable full-choice review overview

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './mobile.css'
+import './review-enhancer.css'
+import './review-enhancer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
